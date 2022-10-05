b09c9a012257db8cd535212824e243d9c2a42f08
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-10-05 17:01:21 -0300

    fix

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -12,7 +12,7 @@ import * as signalR from '@microsoft/signalr';
 })
 export class RequestService {
 
-  public readonly BACKEND_BASE_URL: string = "https://localhost:7042"
+  public readonly BACKEND_BASE_URL: string = "https://tsb-portal.herokuapp.com"
 
   constructor(private httpClient: HttpClient, private dataManager: DataManagerService) {}
 
