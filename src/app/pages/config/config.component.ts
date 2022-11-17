a998c734c6d7b201899aee1f2fed42529099d1c6
Dudu-Arsenal <edudocross@gmail.com>
2022-11-17 01:01:22 -0300

    Services style e Correções

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,22 +1,23 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Infos } from 'src/app/models/Infos.model';
 import { LayoutService } from 'src/app/services/layout.service';
 import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.scss'],
 })
-export class ConfigComponent {
+export class ConfigComponent implements OnInit{
 
   public inputs: ElementRef[] = [];
 
   public infos: Observable<Infos>;
 
-  constructor(public userService: UserService) {
+  constructor(public userService: UserService, private route: ActivatedRoute) {
     userService.infos$.pipe(
       tap(infos => {        
         this.infos = of(infos);
@@ -24,7 +25,18 @@ export class ConfigComponent {
     ).subscribe();
   }  
 
-  ngAfterViewInit() {
+  ngOnInit() {
+    this.route.queryParams.pipe(
+      tap( parametros => {
+        if (parametros.scroll == 'mensalidade'){
+          window.scrollTo({
+            top: 500,
+            behavior: 'smooth'
+          });
+        }
+        return parametros;
+      })
+    ).subscribe()
   }
 
   onSubmit() {    
